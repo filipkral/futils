a7cb1cd815c7d3c2826288b0bdd63a143eb17264
filipkral <filipking@gmail.com>
2013-02-14 03:06:42 +0000

    Cleanup and encapsulate body into main()

--- adding_strings_to_filenames.py
+++ adding_strings_to_filenames.py
@@ -5,7 +5,6 @@ import sys
 
 def GuessDirSeparator(strpath):
     """return a directory separator likely used in strpath, return os.sep if no separator seems to be used"""
-    import os
     osep = os.sep
     if(strpath.find(osep)>=0):
         return osep
@@ -17,7 +16,6 @@ def GuessDirSeparator(strpath):
 
 def AddStringsToFileName(filepath=r"path\to\a\file.ext", addbefore="", addafter=""):
     """Return filepath so that r"path\to\a\file.ext" is returned as r"path\to\a\"+addbefore+"file"+addafter+"ext"""
-    import os
     separator = GuessDirSeparator(filepath)
     base = os.path.basename(filepath)
     dname = os.path.dirname(filepath)
@@ -35,42 +33,48 @@ def locate(pattern, topdir):
         for filename in fnmatch.filter(files, pattern):
             yield os.path.join(path, filename)
 
-try:
-    #
-    # USER INPUT
-    top = r"C:\path\to\toppath" # root folder to start in
-    pattern = "*" # only files like this
-    pre = "before_" # to append before file name
-    post = "_after" # to append before file name
-    logpath = r"C:\temp\logme.txt" # path to a logfile
-    # USER INPUT END
-    #
-    logsep = "\t"
-    errorfreerun = True
-    logfile = open(logpath, "w")
-    #
-    ff = locate(pattern, top)
-    for f in ff:
-        #print "renaming: " + str(f)
-        try:
-            newname = AddStringsToFileName(f, pre, post)
-            logfile.write(f + logsep)
-            os.rename(f, newname) # rename it
-            logfile.write(newname)
-        except Exception, e:
-            logfille.write("Error: " + str(e))
-            errorfreerun = False
-        finally:
-            logfile.write("\n")
-    #
-    if not errorfreerun:
-        gp.addwarning("There were some errors! See "+ logpath + " for details!")
-    #
-except:
-    tb = sys.exc_info()[2]
-    tbinfo = traceback.format_tb(tb)[0]
-    pymsg = tbinfo + "\n" + str(sys.exc_type)+ ": " + str(sys.exc_value)
-    print pymsg
-finally:
-    if "logfile" in dir():
-        logfile.close()
+def main(top = r"C:\path\to\toppath", pattern = "*", pre = "before_",  post = "_after", logpath = r"C:\temp\logme.txt"):
+    """
+    Rename all files under *top* folder (recursively) that match *pattern* so that the new file name si *pre*+originalfilename+*post*+.ext.
+    top: root folder to start in
+    pattern: rename only files that match this pattern, use e.g. *.jpg to deal with jpg files only, use * to include all files
+    pre: string to append at the beginning of a file name
+    post: string to append at the end of a file name
+    logpath: path to a logfile
+    """
+
+    try:
+        logsep = "\t"
+        errorfreerun = True
+        logfile = open(logpath, "w")
+        #
+        ff = locate(pattern, top)
+        for f in ff:
+            #print "renaming: " + str(f)
+            try:
+                newname = AddStringsToFileName(f, pre, post)
+                logfile.write(f + logsep)
+                os.rename(f, newname) # rename it
+                logfile.write(newname)
+            except Exception, e:
+                logfille.write("Error: " + str(e))
+                errorfreerun = False
+            finally:
+                logfile.write("\n")
+        #
+        if not errorfreerun:
+            print "There were some errors! See "+ logpath + " for details!"
+        #
+    except:
+        tb = sys.exc_info()[2]
+        tbinfo = traceback.format_tb(tb)[0]
+        pymsg = tbinfo + "\n" + str(sys.exc_type)+ ": " + str(sys.exc_value)
+        print pymsg
+    finally:
+        if "logfile" in dir():
+            logfile.close()
+
+if __name__ == '__main__':
+    pass
+    #example use:
+    #main(top = r"C:\path\to\top\dir", pattern = "*", pre = "before_",  post = "_after", logpath = r"C:\temp\logme.txt")
