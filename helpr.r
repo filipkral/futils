06c7c26388f5e27e0f1b95f2235c13a905019157
Filip Kral <filipking@gmail.com>
2013-02-15 22:40:34 +0000

    update dataframe2htmltable in helpr.r
    Implement using th tag for column names of input data frame.

--- helpr.r
+++ helpr.r
@@ -1,5 +1,5 @@
-dataframe2htmltable<-function(x, file, htmlclass="", htmlid="", htmlhead="<head><title>Table</title></head>", doctypeline="<!DOCTYPE html>", tableonly=F){
-  # Write data frame to html file and return path to the file on success
+dataframe2htmltable<-function(x, file, htmlclass="", htmlid="", htmlhead="<head><title>Table</title></head>", doctypeline="<!DOCTYPE html>", stylestring="", tableonly=F, useth=F){
+	# Write data frame to html file and return path to the file on success
 	# Note that no formatting is applied to numbers, x has to be formatted already as you want it to appear
 	# x: input data frame
 	# file: output html file
@@ -7,7 +7,9 @@ dataframe2htmltable<-function(x, file, htmlclass="", htmlid="", htmlhead="<head>
 	# htmlid: optional id to be used for the table
 	# htmhead: optional string to be used in place of html "<head>...</head>"
 	# doctypeline: optional doctype line
+	# stylestring: can be used to include inline css style; no style added by default, use for example stylestring="border: 1px solid black"
 	# tableonly: default is F which will write a full html page, only "<table>...</table>" will be written if tableonly=T
+	# useth: default is F, names of columns will not be added; useth will add column names as <tr><th>...</th></tr>
 	if(!tableonly){
 		cat(paste(doctypeline, "\n", htmlhead, sep=""), file=file)
 	}
@@ -20,8 +22,11 @@ dataframe2htmltable<-function(x, file, htmlclass="", htmlid="", htmlhead="<head>
 		idstring<-paste(" id=\"", htmlid, "\"", sep="")
 	}
 	cat(paste("\n<table", classstring, idstring, ">", sep=""), file=file, append=(!tableonly))
+	if(useth){
+		cat(paste("\n<tr>", paste("<th>", names(x), "</th>", sep="", collapse=""), "</tr>", sep=""), file=file, append=T)
+	}
 	for(i in 1:nrow(x)){
-			cat(paste("\n<tr>", paste("<td>", d[i,], "</td>", sep="", collapse=""), "</tr>", sep=""), file=file, append=T)
+			cat(paste("\n<tr>", paste("<td>", x[i,], "</td>", sep="", collapse=""), "</tr>", sep=""), file=file, append=T)
 	}
 	cat(paste("\n</table>", sep=""), file=file, append=T)
 	if(!tableonly){
@@ -29,3 +34,4 @@ dataframe2htmltable<-function(x, file, htmlclass="", htmlid="", htmlhead="<head>
 	}
 	return(file)
 }
+
