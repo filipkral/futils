bbe11eac1d27330976487f92d620a04e54134bfa
Filip Kral <filipking@gmail.com>
2014-11-01 18:32:49 +0000

    Move import time to gloabal scope

--- helpr.py
+++ helpr.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
-
 """Little helper functions for Python"""
+import time
 
 def getTimestampString(form='%Y%m%d%H%M'):
     """Return a string representing current GMT time on local machine in predefined format form, default is yyyymmddHHMM.
     See http://docs.python.org/2/library/time.html#time.strftime for details about time formatting.
     """
-    import time
     return time.strftime(form, time.gmtime())
