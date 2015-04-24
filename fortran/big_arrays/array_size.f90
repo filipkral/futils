4875551b3460d0809fc36ceaea159e49313de3b0
Filip Kral <filipking@gmail.com>
2015-04-24 20:09:09 +0000

    Fix 130K in y dimension to 1300K

--- fortran/big_arrays/array_size.f90
+++ fortran/big_arrays/array_size.f90
@@ -5,11 +5,16 @@ program bigarray
     
     integer, allocatable, dimension(:,:) :: a
     real(kind=8), allocatable, dimension(:,:) :: b
+    real(kind=8), allocatable, dimension(:) :: c
     
     !!integer :: n = 100000
     !!allocate(a(n,n))
     
-    allocate(a(700000/3,130000/3)) !! 70000/3, 1300000/3 (UK at 3 m) integer
-    allocate(b(700000/4,130000/4)) !! 70000/4, 1300000/4 (UK at 4 m) real8
+    !!allocate(a(700000/10,1300000/10)) !! 70000/3, 1300000/3 (UK at 3 m) integer
+    
+    allocate(b(700000/20,1300000/20)) !! 70000/4, 1300000/4 (UK at 4 m) real8
+    !!write(*,*) (700000/4) !*(1307000/4)
+    !!integer :: n = (700000/4)*(130000/4)
+    !!allocate(c(700000)) !! 70000/4*1300000/4
 
 end program bigarray
